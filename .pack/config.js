313413e66b140053e44f5208ecf4c9a461270370
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2022-01-16 20:24:14 +0200

    [feature/19] Create webpack config (#20)

---
+++ .pack/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  target: 'node16',
+  mode: 'development',
+  devtool: 'source-map',
+  externals: [
+    require('webpack-node-externals')()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /\(node_modules\)/,
+        use: {
+          loader: 'swc-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.mjs'
+    ],
+    alias: {
+      src: require('path').resolve(__dirname, '..', 'src'),
+    },
+  },
+  output: {
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map'
+  }
+}
+
