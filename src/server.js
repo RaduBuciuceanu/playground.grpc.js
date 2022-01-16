f027fa9d8747782b9e8b1449f7fe94ca33bbbcdf
radu <radu.buciuceanu@gmail.com>
2022-01-16 17:16:36 +0200

    [feature/7] Create grpc server

---
+++ src/server.js
@@ -0,0 +1,43 @@
+const grpc = require('@grpc/grpc-js')
+const loader = require('@grpc/proto-loader')
+
+const definition = loader.loadSync('src/identity.proto')
+
+const Auth = grpc.loadPackageDefinition(definition)['Identity']['Auth']
+
+const server = new grpc.Server()
+
+const PORT = '0.0.0.0:4000'
+
+const CREDENTIALS = grpc.ServerCredentials.createInsecure()
+
+const setup = () => {
+  setupAuthService()
+  server.start()
+}
+
+server.bindAsync(PORT, CREDENTIALS, setup)
+
+const setupAuthService = () => server.addService(Auth.service, {
+  Login: (_call, callback) => {
+    console.log('login')
+    callback(null, '')
+  },
+  Refresh: (_call, callback) => {
+    console.log('refresh')
+    callback(null, '')
+  },
+  Logout: (_call, callback) => {
+    console.log('logout')
+    callback(null, '')
+  },
+  UserConnected: (call, _callback) => {
+    call.write({ userName: 'zero' })
+    call.write({ userName: 'one' })
+    call.write({ userName: 'two' })
+    call.write({ userName: 'three' })
+    call.write({ userName: 'four' })
+    call.write({ userName: 'five' })
+    call.end()
+  }
+})
